1c38aaf7a34e7ac53e65e69f3e6fc0174468f9c1
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-05-14 20:01:53 +0300

    create RegisterForm.jsx

---
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -0,0 +1,42 @@
+import { Button } from 'components/Button/Button.styled';
+import { BoxForm, Form } from 'components/LoginForm/LoginForm.styled';
+import { useDispatch } from 'react-redux';
+import { register } from 'redux/auth/operations';
+
+export const RegisterForm = () => {
+  const dispatch = useDispatch();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    dispatch(
+      register({
+        name: form.elements.name.value,
+        email: form.elements.email.value,
+        password: form.elements.password.value,
+      })
+    );
+    form.reset();
+  };
+
+  return (
+    <Form onSubmit={handleSubmit} autoComplete="off">
+      <BoxForm>
+        <div>
+          <label htmlFor="name">Username</label>
+          <input type="text" id="name" name="name" required />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" required />
+        </div>
+
+        <Button type="submit">Register</Button>
+      </BoxForm>
+    </Form>
+  );
+};
